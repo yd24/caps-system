9d01b6c9e5bc00d1f00c2ba801a4e6321928252b
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-04-19 22:46:35 -0700

    Start integrating queue to store multiple payloads for clients

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const { Server } = require('socket.io');
 const PORT = process.env.PORT || 3001;
 const io = new Server(PORT);
 const eventPool = require('../eventPool');
+const EventQueue = require('./lib/queue');
 
 let caps = io.of('/caps');
+let eventQueue = new EventQueue();
 
 //Check if client is connected.
 caps.on('connection', (socket) => {
@@ -20,13 +22,22 @@ caps.on('connection', (socket) => {
   //connected socket is listening for pickup
   socket.on(eventPool[0], (payload) => {
     console.log(`${payload.store} has prepared order ${payload.orderId}`);
+    let orderQueue = eventQueue.read(payload.store);
+    if (orderQueue) {
+      orderQueue.store(payload.orderId, payload);
+    } else {
+      let orderQueue = new EventQueue();
+      orderQueue.store(payload.orderId, payload);
+      eventQueue.store(payload.store, orderQueue);
+    }
     socket.broadcast.emit(eventPool[0], payload);
   });
 
   //only DriverSocket emits this
   //in-transit
   socket.on(eventPool[1], (payload) => {
-    caps.to(payload.store).emit(eventPool[1], payload);
+    let orders = eventQueue.read(payload.store);
+    caps.to(payload.store).emit(eventPool[1], orders);
   });
 
   //only DriverSocket emits this
